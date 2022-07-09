c66ddf7144bdd18cd093f89c4d09205e2e2cac13
Rene <renebitter@gmail.com>
2022-07-09 19:11:10 -0300

    Add framer-motion animation

--- components/projects/allProjects.js
+++ components/projects/allProjects.js
@@ -1,6 +1,7 @@
-import { useEffect, useState } from 'react';
-import Link from 'next/link';
 import classes from './allProjects.module.scss';
+import { useState } from 'react';
+import Link from 'next/link';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const AllProjects = (props) => {
   const { projects } = props;
@@ -75,44 +76,55 @@ const AllProjects = (props) => {
 
         <div className={classes.galleryWrap}>
           <div className={classes.gallery}>
-            {filteredProjects.map((project) => (
-              <div key={project._id} className={classes.card}>
-                <div className={classes.cardContent}>
-                  <h4>{project.title}</h4>
-                  <small>
-                    {/* TODO: checks if array, because data is currently mixed with strings */}
-                    {Array.isArray(project.subtitle)
-                      ? project.subtitle.join(', ')
-                      : project.subtitle}
-                  </small>
-                  <p>{project.description}</p>
-                </div>
+            <AnimatePresence>
+              {filteredProjects.map((project) => (
+                <motion.div
+                  animate={{ opacity: 1 }}
+                  initial={{ opacity: 0 }}
+                  exit={{ scale: 0.1 }}
+                  layout
+                  key={project._id}
+                  className={classes.card}>
+                  <div className={classes.cardContent}>
+                    <h4>{project.title}</h4>
+                    <small>
+                      {/* TODO: checks if array, because data is currently mixed with strings */}
+                      {Array.isArray(project.subtitle)
+                        ? project.subtitle.join(', ')
+                        : project.subtitle}
+                    </small>
+                    <p>{project.description}</p>
+                  </div>
 
-                <div className={classes.portfolioLinks}>
-                  {project.githubLink && (
-                    <a
-                      href={project.githubLink}
-                      target='_blank'
-                      rel='noreferrer'>
-                      <i className='fab fa-github'></i>
-                      Github
-                    </a>
-                  )}
-                  {project.liveLink && (
-                    <a href={project.liveLink} target='_blank' rel='noreferrer'>
-                      <i className='fas fa-link'></i>
-                      Website
-                    </a>
-                  )}
-                  <Link href={`/projects/${project._id}`}>
-                    <a>
-                      <i className='fa fa-circle-info'></i>
-                      Project details
-                    </a>
-                  </Link>
-                </div>
-              </div>
-            ))}
+                  <div className={classes.portfolioLinks}>
+                    {project.githubLink && (
+                      <a
+                        href={project.githubLink}
+                        target='_blank'
+                        rel='noreferrer'>
+                        <i className='fab fa-github'></i>
+                        Github
+                      </a>
+                    )}
+                    {project.liveLink && (
+                      <a
+                        href={project.liveLink}
+                        target='_blank'
+                        rel='noreferrer'>
+                        <i className='fas fa-link'></i>
+                        Website
+                      </a>
+                    )}
+                    <Link href={`/projects/${project._id}`}>
+                      <a>
+                        <i className='fa fa-circle-info'></i>
+                        Project details
+                      </a>
+                    </Link>
+                  </div>
+                </motion.div>
+              ))}
+            </AnimatePresence>
           </div>
         </div>
       </div>
