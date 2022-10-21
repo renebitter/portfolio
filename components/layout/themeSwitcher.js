3943dad9f1bc4719f5719a65a713755610cd4b44
Rene <renebitter@gmail.com>
2022-10-21 08:14:01 -0300

    Cleanup

--- components/layout/themeSwitcher.js
+++ components/layout/themeSwitcher.js
@@ -1,28 +1,23 @@
-import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 
 const ThemeSwitcher = (props) => {
   const { theme } = props;
 
   return (
-    <div>
-      <motion.div layout className='handle'>
-        <AnimatePresence exitBeforeEnter initial={false}>
-          <motion.i
-            className={`fa ${theme === 'light' ? 'fa-moon' : 'fa-sun'}`}
-            key={theme === 'light' ? 'moon' : 'sun'}
-            initial={{ y: -30, opacity: 0, rotate: 50 }}
-            animate={{
-              y: 0,
-              opacity: 1,
-              rotate: 360,
-            }}
-            exit={{ y: 30, opacity: 0 }}
-            transition={{ duration: 0.2 }}
-          />
-        </AnimatePresence>
-      </motion.div>
-    </div>
+    <AnimatePresence exitBeforeEnter>
+      <motion.i
+        className={`fa ${theme === 'light' ? 'fa-moon' : 'fa-sun'}`}
+        key={theme === 'light' ? 'moon' : 'sun'}
+        initial={{ x: -20, y: 15, opacity: 1 }}
+        animate={{
+          x: 0,
+          y: 0,
+          opacity: 1,
+        }}
+        exit={{ x: 20, y: 15, opacity: 0 }}
+        transition={{ ease: 'easeOut', duration: 0.4 }}
+      />
+    </AnimatePresence>
   );
 };
 
